5899d96ed11fee40193709cacb6ce658b1089b26
anund <anund@anunds-Air.home>
2024-12-27 23:29:59 +0100

    add nvim

---
+++ nvim/lua/kickstart/plugins/noice.lua
@@ -0,0 +1,40 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	enabled = true,
+	opts = {
+
+		routes = {
+			{
+				filter = {
+					event = "msg_show",
+					any = {
+						{ find = "%d+L, %d+B" },
+						{ find = "; after #%d+" },
+						{ find = "; before #%d+" },
+					},
+				},
+				opts = { skip = true },
+			},
+			{
+				view = "notify",
+				filter = { event = "msg_showmode" },
+			},
+		},
+		lsp = {
+			hover = {
+				silent = true,
+			},
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+		presets = {
+			bottom_search = false,
+			command_palette = true,
+			long_message_to_split = true,
+		},
+	},
+}
---
+++ nvim/lua/kickstart/plugins/toggleterm.lua
@@ -0,0 +1,37 @@
+-- Close Terminal and open buffer in "normal window".
+local function go_to_file()
+	local toggleterm = require("toggleterm")
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	local bufnr = vim.api.nvim_get_current_buf()
+	toggleterm.toggle(0)
+	vim.api.nvim_win_set_buf(0, bufnr)
+	vim.api.nvim_win_set_cursor(0, cursor)
+	vim.cmd("norm! gf")
+end
+
+vim.api.nvim_create_augroup("ToggleTerm", {})
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "term://*toggleterm#*",
+	callback = function()
+		vim.keymap.set("n", "gf", go_to_file, { buffer = true })
+		-- Normal mode in terminal
+		vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { buffer = true })
+	end,
+	group = "ToggleTerm",
+})
+
+return {
+	"akinsho/toggleterm.nvim",
+	event = "VeryLazy",
+	opts = {
+		direction = "float",
+		size = 60,
+		insert_mappings = false,
+		start_in_insert = true,
+		persist_mode = false,
+		open_mapping = [[<leader>+]],
+		float_opts = {
+			border = "curved",
+		},
+	},
+}
